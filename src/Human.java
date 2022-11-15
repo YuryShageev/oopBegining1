466b3e8f4718120867d994c6bbfa331b2e091eef
YuryShageev <schageev.yur@yandex.ru>
2022-11-15 17:11:13 +0300

    Первые шаги в ООП

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,18 @@
 public class Human {
 
     int yearOfBirth;
+    String name;
+    String town;
+
+    void meetPerson(int yearOfBirth, String name, String town) {
+        this.name = name;
+        this.town = town;
+        this.yearOfBirth = yearOfBirth;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности. Будем знакомы!";
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,9 @@
 public class main {
+    public static void main(String[] args) {
+        Human meetPerson1 = new Human();
+        meetPerson1.yearOfBirth = 1988;
+        meetPerson1.town = "Минск";
+        meetPerson1.name = "Максим";
+        System.out.println(meetPerson1);
+    }
 }
