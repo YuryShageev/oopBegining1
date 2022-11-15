db4842e826828912b2510d3b8410fa4c3c391365
YuryShageev <schageev.yur@yandex.ru>
2022-11-15 17:46:50 +0300

    Первые шаги в ООП

--- src/Human.java
+++ src/Human.java
@@ -3,16 +3,17 @@ public class Human {
     int yearOfBirth;
     String name;
     String town;
+    String jobTitle;
 
-    void meetPerson(int yearOfBirth, String name, String town) {
+    void meetPerson(int yearOfBirth, String name, String town, String jobTitle) {
         this.name = name;
         this.town = town;
         this.yearOfBirth = yearOfBirth;
-
+        this.jobTitle = jobTitle;
     }
 
     @Override
     public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности. Будем знакомы!";
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
     }
 }
--- src/main.java
+++ src/main.java
@@ -4,24 +4,28 @@ public class main {
         meetPerson1.yearOfBirth = 1988;
         meetPerson1.town = "Минск";
         meetPerson1.name = "Максим";
+        meetPerson1.jobTitle = "Бренд Менеджер";
         System.out.println(meetPerson1);
 
         Human meetPerson2 = new Human();
         meetPerson2.yearOfBirth = 1993;
         meetPerson2.town = "Москва";
         meetPerson2.name = "Аня";
+        meetPerson2.jobTitle = "Методист образовательных программ";
         System.out.println(meetPerson2);
 
         Human meetPerson3 = new Human();
         meetPerson3.yearOfBirth = 1992;
         meetPerson3.town = "Калининград";
         meetPerson3.name = "Катя";
+        meetPerson3.jobTitle = "Продакт-Менеджер";
         System.out.println(meetPerson3);
 
         Human meetPerson4 = new Human();
         meetPerson4.yearOfBirth = 1995;
         meetPerson4.town = "Москва";
         meetPerson4.name = "Артём";
+        meetPerson4.jobTitle = "Директор по развитию бизнеса";
         System.out.println(meetPerson4);
     }
 }
