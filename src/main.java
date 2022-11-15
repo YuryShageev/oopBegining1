6caf1388d66aa257963a154654f5779e3f77d819
YuryShageev <schageev.yur@yandex.ru>
2022-11-15 17:41:48 +0300

    Первые шаги в ООП

--- src/main.java
+++ src/main.java
@@ -5,5 +5,23 @@ public class main {
         meetPerson1.town = "Минск";
         meetPerson1.name = "Максим";
         System.out.println(meetPerson1);
+
+        Human meetPerson2 = new Human();
+        meetPerson2.yearOfBirth = 1993;
+        meetPerson2.town = "Москва";
+        meetPerson2.name = "Аня";
+        System.out.println(meetPerson2);
+
+        Human meetPerson3 = new Human();
+        meetPerson3.yearOfBirth = 1992;
+        meetPerson3.town = "Калининград";
+        meetPerson3.name = "Катя";
+        System.out.println(meetPerson3);
+
+        Human meetPerson4 = new Human();
+        meetPerson4.yearOfBirth = 1995;
+        meetPerson4.town = "Москва";
+        meetPerson4.name = "Артём";
+        System.out.println(meetPerson4);
     }
 }
